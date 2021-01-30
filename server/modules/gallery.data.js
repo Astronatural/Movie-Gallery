bdf20cd9db18a62bc108d16d9fe80ff6db6bfa6d
JDK <jdkranzler@yahoo.com>
2021-01-30 12:29:44 -0600

    added posters

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/robin_hood.jpg', description: 'My first favorite movie.  Still my most watched, probably.', likes: 0 },
+    { id: 3, path: 'images/night-of-the-living-dead.jpg', description: 'First scary movie I liked and still a favorite today.', likes: 0 },
+    { id: 4, path: 'images/road_warrior.jpg', description: 'Fantastic movie with better dialouge than an action film deserves.', likes: 0 },
+    { id: 5, path: 'images/seven_samurai.jpg', description: 'One of the rare 3-hour movies that does not feel "long" to me.', likes: 0 },
+    { id: 6, path: 'images/Spirited_Away.jpg', description: 'I have always loved animated films and this is one of the best.', likes: 0 },
+    { id: 7, path: 'images/phantom.jpg', description: 'It is cheezy but I have loved it for a long time now.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
