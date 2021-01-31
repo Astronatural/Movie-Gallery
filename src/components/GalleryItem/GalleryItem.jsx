13907099aa7c68c6f9c48c8641b3949820260922
JDK <jdkranzler@yahoo.com>
2021-01-31 12:19:22 -0600

    work on picSwap

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -5,10 +5,29 @@ function GalleryItem({ galleryItems, setLike }) {
 //  Set up a useState to check clicks on the posters.
 //  switch between posters and descriptions.
 //  can I just shove the poster in a button?
-    const [clickState, setClickState]  = useState(true);
+    const [clickState, setClickState]  = useState(true);  // hook for pic swap.
+
+    const togglePic = () => {
+        console.log('in togglePic');
+        setClickState(!clickState);
+    }
+    
+    
+    // build tags for elements
+    const picSwap = () => {
+     if (clickState) {
+         return  <img src={galleryItems.path} width="15%" height="20%"></img>
+     }
+     else {
+           return <p>{galleryItems.description}</p>
+    }
+    }
+   
+
 
     return (
         <>
+            {picSwap()}
         <img src={galleryItems.path} width="15%" height="20%"></img>
             <p>{galleryItems.likes} People love this film too!</p>
             <button onClick={()=>setLike(galleryItems.id)}>My Fav too!</button>
