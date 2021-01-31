bce44a9881affcb09f428b101867def7546c9368
JDK <jdkranzler@yahoo.com>
2021-01-30 21:59:15 -0600

    start of last base goal

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,11 @@
-
+import React, {useState} from 'react';
 
 function GalleryItem({ galleryItems, setLike }) {
 
+//  Set up a useState to check clicks on the posters.
+//  switch between posters and descriptions.
+//  can I just shove the poster in a button?
+    const [clickState, setClickState]  = useState(true);
 
     return (
         <>
